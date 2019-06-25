14011d674fc326e88fb0bbb2cdeccc3311022c03
Max Beutelspacher <max.beutelspacher@mailbox.org>
2019-06-25 21:04:02 +0200

    format

--- habit/store.py
+++ habit/store.py
@@ -1,8 +1,9 @@
 import os
 from git import Repo
 
+
 class DataStore:
-    def __init__(self,path):
+    def __init__(self, path):
         if not os.path.exists(path):
             raise FileNotFoundError('{} does not exist'.format(path))
         if not os.path.isdir(path):
--- tests/test_store.py
+++ tests/test_store.py
@@ -15,22 +15,27 @@ def test_datastore_raises_error_if_directory_does_not_exists():
     with pytest.raises(FileNotFoundError):
         _ = DataStore(path)
 
+
 def test_datastore_raises_error_if_path_is_not_a_directory():
-    _,path = tempfile.mkstemp()
+    _, path = tempfile.mkstemp()
     with pytest.raises(NotADirectoryError):
         _ = DataStore(path)
     os.remove(path)
 
+
 @pytest.fixture
 def datastore():
     path = tempfile.mkdtemp()
+
     def fin():
         shutil.rmtree(path)
+
     return DataStore(path)
 
 
 def test_datastore_can_be_initialized_with_an_existing_directory(datastore):
     pass
 
+
 def test_datastore_creates_git_repository_at_path(datastore):
-    assert os.path.isdir(os.path.join(datastore.path,'.git'))
+    assert os.path.isdir(os.path.join(datastore.path, '.git'))
