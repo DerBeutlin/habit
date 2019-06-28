103d7e6c96d60913aa741ef8e2866aaac18d45d3
Max Beutelspacher <max.beutelspacher@mailbox.org>
2019-06-28 07:35:24 +0200

    test create goal in cli

--- habit/cli.py
+++ habit/cli.py
@@ -2,6 +2,7 @@
 """Console script for habit."""
 import click
 from habit.store import DataStore
+from habit.goal import create_goal
 import os
 
 
@@ -9,6 +10,17 @@ import os
 def main(args=None):
     pass
 
+
 @main.command()
 def init():
     DataStore.init(os.getcwd())
+
+
+@main.command()
+@click.argument('name')
+@click.option('--slope', 'slope', default=1, help='Daily value increase')
+@click.option('--pledge', 'pledge', default=0, help='Starting Pledge')
+def new(name, slope, pledge):
+    store = DataStore(os.getcwd())
+    goal = create_goal(name=name, daily_slope=slope, pledge=pledge)
+    store.add_goal(goal)
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,14 +1,46 @@
 from click.testing import CliRunner
 from habit.cli import main
+from habit.goal import Goal, create_goal
 import os
 import pytest
+
 
 @pytest.fixture
 def runner():
     return CliRunner()
 
+
 def test_init_creates_a_git_repository(runner):
     with runner.isolated_filesystem():
-        result = runner.invoke(main,['init'])
+        result = runner.invoke(main, ['init'])
         assert result.exit_code == 0
         assert os.path.exists('.git')
+
+
+def test_add_goal(runner):
+    with runner.isolated_filesystem():
+        result = runner.invoke(main, ['init'])
+        result = runner.invoke(main, ['new', 'dummy'])
+        assert result.exit_code == 0
+        assert os.path.exists('dummy.yaml')
+        goal = Goal.fromYAML('dummy.yaml')
+        assert goal.name == 'dummy'
+        assert goal.pledge == 0
+        same_goal = create_goal(name='dummy', daily_slope=1, pledge=0)
+        assert goal.reference_points[1].value == same_goal.reference_points[
+            1].value
+
+
+def test_add_goal_with_parameter(runner):
+    with runner.isolated_filesystem():
+        result = runner.invoke(main, ['init'])
+        result = runner.invoke(
+            main, ['new', 'dummy', '--slope', '10', '--pledge', '10'])
+        assert result.exit_code == 0
+        assert os.path.exists('dummy.yaml')
+        goal = Goal.fromYAML('dummy.yaml')
+        assert goal.name == 'dummy'
+        assert goal.pledge == 10
+        same_goal = create_goal(name='dummy', daily_slope=10, pledge=0)
+        assert goal.reference_points[1].value == same_goal.reference_points[
+            1].value
